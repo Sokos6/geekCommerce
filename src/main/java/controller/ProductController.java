fdd826f02310b95f8769777a582432f5098a7fe9
Sokos6 <will@comicbooked.com>
2016-01-28 17:37:29 -0700

    package configured incorrectly

--- src/main/java/controller/ProductController.java
+++ src/main/java/controller/ProductController.java
@@ -1,24 +1,25 @@
 package controller;
 
-import java.math.BigDecimal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import domain.Product;
+
+import repository.ProductRepository;
 
 @Controller
-public class ProductController {
 
+
+
+public class ProductController {
+	@Autowired
+	private ProductRepository productRepository;
 	@RequestMapping("/products")
 	public String list(Model model) {
-		Product iphone = new Product("P1234", "iPhone 5s", new BigDecimal(500));
-		iphone.setDescription("Apple iPhone 5s smartphone with 4.00-inch 640x1136 display and 8-megapixel rear camera");
-		iphone.setCategory("Smart Phone");
-		iphone.setManufacturer("Apple");
-		iphone.setUnitsInStock(1000);
-
-		model.addAttribute("product", iphone);
-
-		return "products";
+	  model.addAttribute("products", productRepository.getAllProducts());
+	  return "products";
 	}
+	
+
 }
+
---
+++ src/main/java/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+import java.util.List;
+
+import domain.Product;
+
+public interface ProductRepository {
+
+	List <Product> getAllProducts();
+}
